00e28b60ab273483524a8512afef897c9a366c6c
bogdan778 <leyn778@gmail.com>
2022-06-16 23:25:53 +0300

    add route

--- lib/route.dart
+++ lib/route.dart
@@ -1,14 +1,14 @@
-
-
 import 'package:durudex_mobile/screens/auth/first_screen.dart';
 import 'package:durudex_mobile/screens/auth/sign_in_screen.dart';
 import 'package:durudex_mobile/screens/auth/sign_up_screen.dart';
+import 'package:durudex_mobile/screens/auth/forget_password.dart';
 
 import 'package:flutter/material.dart';
 
 const String first_screen = 'firstScreen';
 const String sign_in_screen = 'SignIn';
 const String sign_up_screen = 'SignUp';
+const String forget_password = 'ForgetPassword';
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
@@ -18,6 +18,8 @@ Route<dynamic> controller(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => const SignInScreen());
     case sign_up_screen:
       return MaterialPageRoute(builder: (context) => const SignUpWidget());
+    case forget_password:
+      return MaterialPageRoute(builder: (context) => const ForgetPassword());
     default:
       throw ("this route doesn't exist");
   }
